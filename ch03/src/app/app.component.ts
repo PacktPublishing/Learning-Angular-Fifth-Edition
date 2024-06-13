d2b1d231a3d3fb85d474c75f29679c96508bcc43
Aristeidis Bampakos <bampakoa@gmail.com>
2024-06-13 22:58:37 +0300

    Upgrade to Angular 18

--- ch03/src/app/app.component.ts
+++ ch03/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProductListComponent],
+  imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
